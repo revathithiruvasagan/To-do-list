732658c61331d8b8223fe57cb9377d7d5e2bc39a
Revathi Thiruvasagan <131807019+revathithiruvasagan@users.noreply.github.com>
2024-06-28 13:32:42 +0530

    Updated
    Updated

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres12",
-  password: "xYVx7dAfRrOTaYDnO1dt7TskqLdEmFn1",
-  host: "dpg-cpv65idumphs73c6br00-a",
+  user: "postgres",
+  password: "remo&rt",
+  host: "localhost",
   port: 5432,
-  database: "todo_75yt",
+  database: "summerproject",
 });
 
 module.exports = pool;
